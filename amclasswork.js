1145fee56f8386536516708fef8e7c3945d191c5
rjrobins16 <rjrobins16@gmail.com>
2019-08-13 12:10:45 -0500

    classwork

---
+++ amclasswork.js
@@ -0,0 +1,15 @@
+let names = ["Rachel", "Malik", "Kenya","Giselle"];
+
+alert(names);
+
+
+let numberArray = [2,4,6,8,10];
+alert(numberArray);
+
+
+
+let namesArray = ["Rachel","Malik","Kenya","Giselle"];
+
+alert(namesArray[2]);
+namesArray.slice([1]);
+alert(namesArray[3]);
