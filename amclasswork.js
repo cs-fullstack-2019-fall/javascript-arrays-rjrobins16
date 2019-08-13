b1c864a12e09294630821268b4a0fa49ffc29530
rjrobins16 <rjrobins16@gmail.com>
2019-08-13 12:12:59 -0500

    classwork

--- amclasswork.js
+++ amclasswork.js
@@ -1,11 +1,27 @@
+
+// Exercise 1
+//1. Create an array.
+//2. Add four names to the array.
+//3. Print the array
+
 let names = ["Rachel", "Malik", "Kenya","Giselle"];
 
 alert(names);
+
+// Exercise 2
+//1. Create an array with 5 numbers in it.
+//2. Print the numbers.
+
 
 
 let numberArray = [2,4,6,8,10];
 alert(numberArray);
 
+//Exercise 3
+//1. Create an array with 4 names.
+//2. Print the third item in the array.
+//. Delete the second item.
+//4. Print the third item in the array again.
 
 
 let namesArray = ["Rachel","Malik","Kenya","Giselle"];
